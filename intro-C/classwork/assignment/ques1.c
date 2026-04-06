a354b1e0d0292a748c4bea0c6f20289e11cae373
Farhan Khan <farhankhan.devv@gmail.com>
2026-04-06 16:30:15 +0530

    completed  question of assignment

---
+++ intro-C/classwork/assignment/ques1.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+void oddOrEven();
+
+int main()
+{
+
+  oddOrEven();
+
+  return 0;
+}
+
+void oddOrEven()
+{
+  // CHECK WHETHER USER GIVEN NO. ODD OR EVEN
+  int userInput;
+  printf("\nEnter an integer: ");
+  scanf("%d", &userInput);
+
+  if (userInput % 2 == 0)
+  {
+    printf("\nThe number %d is even", userInput);
+  }
+  else
+  {
+    printf("\nThe number %d is odd", userInput);
+  }
+}
